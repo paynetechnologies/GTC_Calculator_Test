94edc6818afd246e54988073f7d5c0152bde762e
Howard Payne <paynehd@gmail.com>
2019-03-19 19:01:22 -0400

    Start POM Java to C#

--- POM_Example/PageObjects/AboutPage.cs
+++ POM_Example/PageObjects/AboutPage.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace POMExample.PageObjects
 {
-    class AboutPage
+    public class AboutPage
     {
+        public AboutPage(IWebDriver driver)
+        {
+
+        }
     }
 }
--- POM_Example/PageObjects/HomePage.cs
+++ POM_Example/PageObjects/HomePage.cs
@@ -1,14 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+//using OpenQA.Selenium.Support.PageObjects;
 
 
 namespace POMExample.PageObjects
 {
     class HomePage
     {
-    }
-}
 
+        private IWebDriver driver;
+        private IWebElement about;
+        private IWebElement searchIcon;
+
+        public HomePage(IWebDriver driver)
+        {
+            driver = new ChromeDriver();
+            var cn = driver.FindElement(By.ClassName("fusion-main-menu-icon"));
+            var cs = driver.FindElement(By.CssSelector(".fusion-main-menu a[href*='about']"));
+
+            //this.driver = driver;
+            //[FindsBy(How = How.ClassName, Using = "fusion-main-menu-icon")]
+            //[FindsBy(How = How.CssSelector, Using = ".fusion-main-menu a[href*='about']")]
+            //PageFactory.InitElements(driver, this);
+        }
+        public void goToPage()
+        {
+            driver.Navigate().GoToUrl("http://www.swtestacademy.com");
+        }
+
+        public AboutPage goToAboutPage()
+        {
+            about.Click();
+            return new AboutPage(driver);
+        }
+    } 
+}
